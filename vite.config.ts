85dfc917f832569d5d28e9dec940c41f180c5b7f
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-07-01 21:58:57 -0400

    Enable React in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/sst/',
   publicDir: 'public',
+  plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true
