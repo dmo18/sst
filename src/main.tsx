f72868591658daefa269220a53e17d822d5e29c5
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-07-30 11:08:19 -0400

    Load status UI tweak styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles/app.css';
+import './styles/status-tweaks.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
