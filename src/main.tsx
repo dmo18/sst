5cf98101684951b7d0599948810a65a5d84c4c43
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-07-30 18:21:04 -0400

    Load mobile responsive fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles/app.css';
 import './styles/status-tweaks.css';
+import './styles/mobile.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
