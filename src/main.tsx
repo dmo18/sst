370efad98c4e655e413c5d41aa0c127eaa8d7b9f
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-08-01 22:19:30 -0400

    Load command center design system

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import './styles/app.css';
-import './styles/status-tweaks.css';
-import './styles/mobile.css';
-import './styles/site-guide.css';
-import './styles/intelligence.css';
+import './styles/command-center.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
