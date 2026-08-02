a65816a9675c1d704297a922a3cefcf7530c2a59
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-08-02 01:14:08 -0400

    Load ultra-HD tuning layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles/command-center.css';
 import './styles/ultra-hd.css';
+import './styles/ultra-hd-tuning.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
