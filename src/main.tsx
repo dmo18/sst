deec35386bce0a654f76f0ac86baec297c3c296a
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-08-02 01:06:13 -0400

    Load ultra-HD visual system

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles/command-center.css';
+import './styles/ultra-hd.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
